5bce23f88aa94f2d85e5e378f4097e3e66b51fd0
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-11-03 19:31:50 -0800

    featureOneComplete

--- js/app.js
+++ js/app.js
@@ -1,2 +1,51 @@
-'use strict'
+'use strict';
 
+
+
+function Creatures(creature) {
+
+  this.image_url = creature.image_url;
+  this.title = creature.title;
+  this.description = creature.description;
+  this.keyword = creature.keyword;
+  this.horns = creature.horns;
+
+}
+
+// getting template section from HTML, cloning it, and appending to main section
+Creatures.prototype.render = function () {
+  let $creatureClone = $('<div></div>').clone();
+  $creatureClone.html($('#photo-template').html());
+  // $('main').append($creatureClone);
+
+  // getting a cloned element's specific tags and providing text/url
+  $creatureClone.find('h2').text(this.title);
+  $creatureClone.find('img').attr('src', this.image_url);
+  $creatureClone.find('p').text(this.description);
+  // removing the class to make template render to page
+  $creatureClone.attr('class', this.keyword);
+  $('main').append($creatureClone);
+}
+
+Creatures.allCreatures = [];
+
+Creatures.readJson = () => {
+  const ajaxSetting = { method: 'get', dataType: 'json' };
+
+  $.ajax('../data/page-1.json', ajaxSetting)
+    .then(data => {
+      data.forEach(hornInfo => {
+        Creatures.allCreatures.push(new Creatures(hornInfo));
+      });
+      Creatures.allCreatures.forEach(image => {
+        $('main').append(image.render())
+      })
+      // console.log(animals);
+      // animals.render();
+    });
+}
+
+$(() => Creatures.readJson());
+
+//create global arry for new creatures
+//then, do animals.render
